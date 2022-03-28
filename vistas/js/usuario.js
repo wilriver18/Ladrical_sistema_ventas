e1a3eedb75fc35b29e46201cbe6326455a6db91e
Cristian Hualta Mamani <101910903+CrisHM0703@users.noreply.github.com>
2022-03-27 20:11:21 -0400

    usuario

--- vistas/js/usuario.js
+++ vistas/js/usuario.js
@@ -28,4 +28,178 @@ function init(){
 
 }
 
+//Función limpiar
+function limpiar()
+{
+	$("#idpersonal").val("");
+	$("#login").val("");
+	$("#clave").val("");
+	$("#idusuario").val("");
+
+}
+
+//Función mostrar formulario
+function mostrarform(flag)
+{
+	limpiar();
+	if (flag)
+	{
+		$("#listadoregistros").show();
+		$('#myModal').modal('show');
+	}
+	else
+	{
+		$("#listadoregistros").show();
+		$("#btnagregar").show();
+	}
+}
+
+//Función cancelarform
+function cancelarform()
+{
+	limpiar();
+	mostrarform(false);
+	$.post("../controladores/usuario.php?op=permisos&id=",function(r){
+	        $("#permisos").html(r);
+	});
+}
+
+//Función Listar
+function listar()
+{
+	tabla=$('#tbllistado').dataTable(
+	{
+		"aProcessing": true,//Activamos el procesamiento del datatables
+	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
+	    "processing": true,
+	    "language": 
+		{          
+		"processing": "<img style='width:80px; height:80px;' src='../files/plantilla/loading-page.gif' />",
+		},
+	    dom: 'Bfrtip',//Definimos los elementos del control de tabla
+	    lengthMenu: [
+            [5,10, 25, 50, 100, -1],
+            ['5 filas','10 filas', '25 filas', '50 filas','100 filas', 'Mostrar todo']
+        ],
+        buttons: ['pageLength','copy','excel', 'pdf'],
+		"ajax":
+				{
+					url: '../controladores/usuario.php?op=listar',
+					type : "get",
+					dataType : "json",						
+					error: function(e){
+						console.log(e.responseText);	
+					}
+				},
+		"bDestroy": true,
+		"iDisplayLength": 5,//Paginación
+	    "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+	}).DataTable();
+}
+//Función para guardar o editar
+
+function guardaryeditar(e)
+{
+	e.preventDefault(); //No se activará la acción predeterminada del evento
+	//$("#btnGuardar").prop("disabled",true);
+	var formData = new FormData($("#formulario")[0]);
+
+	$.ajax({
+		url: "../controladores/usuario.php?op=guardaryeditar",
+	    type: "POST",
+	    data: formData,
+	    contentType: false,
+	    processData: false,
+
+	    success: function(datos)
+	    {                    
+	          swal({
+				  title: 'usuario',
+				  type: 'success',
+					text:datos
+				});
+              $('#myModal').modal('hide');	          
+	          mostrarform(false);
+	          tabla.ajax.reload();
+	    }
+
+	});
+	limpiar();
+	
+}
+
+function mostrar(idusuario)
+{
+	$.post("../controladores/usuario.php?op=mostrar",{idusuario : idusuario}, function(data, status)
+	{
+		data = JSON.parse(data);		
+		mostrarform(true);
+		$("#idpersonal").val(data.idpersonal);
+		$('#idpersonal').selectpicker('refresh');
+		$("#login").val(data.login);
+		$("#clave").val(data.clave);
+		$("#idusuario").val(data.idusuario);
+
+ 	});
+
+ 	$.post("../controladores/usuario.php?op=permisos&id="+idusuario,function(r){
+	        $("#permisos").html(r);
+	});
+}
+
+//Función para desactivar registros
+function desactivar(idusuario)
+{
+	swal({
+						    title: "¿Desactivar?",
+						    text: "¿Está seguro Que Desea Desactivar el Usuario?",
+						    type: "warning",
+						    showCancelButton: true,
+								cancelButtonText: "No",
+								cancelButtonColor: '#FF0000',
+						    confirmButtonText: "Si",
+						    confirmButtonColor: "#0004FA",
+						    closeOnConfirm: false,
+						    closeOnCancel: false,
+						    showLoaderOnConfirm: true
+						    },function(isConfirm){
+						    if (isConfirm){
+									$.post("../controladores/usuario.php?op=desactivar", {idusuario : idusuario}, function(e){
+										swal(
+											'!!! Desactivada !!!',e,'success')
+					            tabla.ajax.reload();
+				        	});
+						    }else {
+						    swal("! Cancelado ¡", "Se Cancelo la desactivacion del Usuario", "error");
+							 }
+							});
+}
+
+//Función para activar registros
+function activar(idusuario)
+{
+	swal({
+		    title: "¿Activar?",
+		    text: "¿Está seguro Que desea Activar el Usuario?",
+		    type: "warning",
+		    showCancelButton: true,
+				confirmButtonColor: '#0004FA',
+				confirmButtonText: "Si",
+		    cancelButtonText: "No",
+				cancelButtonColor: '#FF0000',
+		    closeOnConfirm: false,
+		    closeOnCancel: false,
+		    showLoaderOnConfirm: true
+		    },function(isConfirm){
+		    if (isConfirm){
+						$.post("../controladores/usuario.php?op=activar", {idusuario : idusuario}, function(e){
+						swal("!!! Activada !!!", e ,"success");
+								tabla.ajax.reload();
+						});
+		    }else {
+		    swal("! Cancelado ¡", "Se Cancelo la activacion del Usuario", "error");
+			 }
+			});
+}
+
 init();
